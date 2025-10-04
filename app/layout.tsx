918e771d0f311a1dbf895811dfa7a22c8134f38d
mcapace <mike.capace@gmail.com>
2025-10-04 18:08:04 -0400

    Add Vercel Analytics to the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import CookieBanner from "./components/CookieBanner";
+import { Analytics } from '@vercel/analytics/react';
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -45,6 +46,7 @@ export default function RootLayout({
       <body className="font-sans antialiased">
         {children}
         <CookieBanner />
+        <Analytics />
       </body>
     </html>
   );
