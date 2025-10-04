4d4fb2c4b44d28d16547e328714a3efa5fb866c6
mcapace <mike.capace@gmail.com>
2025-10-04 18:21:39 -0400

    Add favicon setup with multiple sizes and proper meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,6 +87,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon-192.png" type="image/png" sizes="192x192" />
+        <link rel="icon" href="/icon-512.png" type="image/png" sizes="512x512" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <StructuredData />
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=G-8ZK93TZL47`}
